ec5e4b32aec894f8630afcfdf60a60fa9745df67
repul <repulbehl9@gmail.com>
2023-11-26 01:47:36 +0530

    Main Frame Added

---
+++ newsServices/src/main/java/com/newsCorp/newsServices/NewsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.newsCorp.newsServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsServicesApplication.class, args);
+	}
+
+}
---
+++ newsServices/src/test/java/com/newsCorp/newsServices/NewsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsCorp.newsServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
