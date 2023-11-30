25ac1a62bf54ab3b7c329d4862e8812f72769f3f
repul <repulbehl9@gmail.com>
2023-12-01 00:54:15 +0530

    Data Fetch from External API's Completed

--- newsServices/src/main/java/com/newsCorp/newsServices/endpoints/FavouriteNews.java
+++ newsServices/src/main/java/com/newsCorp/newsServices/endpoints/FavouriteNews.java
@@ -2,17 +2,15 @@ package com.newsCorp.newsServices.endpoints;
 
 
 import com.newsCorp.newsServices.entity.News;
+import com.newsCorp.newsServices.model.FavouriteByUser;
 import com.newsCorp.newsServices.service.FavouriteNewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/favNews")
 public class FavouriteNews {
 
@@ -20,12 +18,13 @@ public class FavouriteNews {
     FavouriteNewsService favouriteNewsService;
 
     @PostMapping("/add/news")
-    public ResponseEntity<News> addFavouriteNews(){
+    public ResponseEntity<News> addFavouriteNews(@RequestBody FavouriteByUser favouriteByUser){
+        // write Logic for adding the news to FavouriteNewsRepo and business logic contains news
         return new ResponseEntity<>(new News(),HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/remove")
-    public  ResponseEntity<News> deleteFavouriteNews(){
+    public  ResponseEntity<News> deleteFavouriteNews(@RequestParam int id){
         return new ResponseEntity<News>(new News(),HttpStatus.ACCEPTED);
     }
 
--- newsServices/src/main/java/com/newsCorp/newsServices/entity/News.java
+++ newsServices/src/main/java/com/newsCorp/newsServices/entity/News.java
@@ -1,7 +1,6 @@
 package com.newsCorp.newsServices.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,14 +11,33 @@ import java.util.List;
 @Table(name = "NEWS_CORP_NEWS")
 public class News {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "NEWS_ID")
+    private Long newsId;
+
+    @Column(name = "TITLE")
     private String title;
+
+    @Column(name = "CONTENT")
     private String content;
+
+    @Column(name = "AUTHOR")
     private String author;
+
+    @Column(name = "CATEGORY")
     private String category;
+
+    @Column(name = "PUBLISH_DATE")
     private Date publishedDate;
+
+    @Column(name = "LIKES")
     private int likes;
-    private int userId;
+
+    @Column(name = "COMMENT_COUNT")
+    private int numberOfComments;
+
+    @OneToMany(targetEntity = Comment.class ,mappedBy = "news",cascade = CascadeType.ALL)
     private List<Comment> comments;
 
 }
